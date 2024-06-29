859f0d39990a6b35146b0ed1e305a25d0d27ec4b
watermeleon <leoneshuijs@gmail.com>
2024-06-29 15:07:02 +0200

    predict trigger token first setup working

---
+++ src/llm_attacks_mod/opt_utils.py
@@ -0,0 +1,71 @@
+import gc
+
+import numpy as np
+import torch
+import torch.nn as nn
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+# from llm_attacks import get_embedding_matrix, get_embeddings
+from src.llm_attacks_mod.attack_manager import get_embedding_matrix, get_embeddings
+
+
+def token_gradients(model, input_ids, input_slice, target_slice, loss_slice):
+
+    """
+    Computes gradients of the loss with respect to the coordinates.
+
+    Parameters
+    ----------
+    model : Transformer Model
+        The transformer model to be used.
+    input_ids : torch.Tensor
+        The input sequence in the form of token ids.
+    input_slice : slice
+        The slice of the input sequence for which gradients need to be computed.
+    target_slice : slice
+        The slice of the input sequence to be used as targets.
+    loss_slice : slice
+        The slice of the logits to be used for computing the loss.
+
+    Returns
+    -------
+    torch.Tensor
+        The gradients of each token in the input_slice with respect to the loss.
+    """
+
+    embed_weights = get_embedding_matrix(model)
+    one_hot = torch.zeros(
+        input_ids[input_slice].shape[0],
+        embed_weights.shape[0],
+        device=model.device,
+        dtype=embed_weights.dtype,
+    )
+    one_hot.scatter_(
+        1,
+        input_ids[input_slice].unsqueeze(1),
+        torch.ones(one_hot.shape[0], 1, device=model.device, dtype=embed_weights.dtype),
+    )
+    one_hot.requires_grad_()
+    input_embeds = (one_hot @ embed_weights).unsqueeze(0)
+
+    # now stitch it together with the rest of the embeddings
+    embeds = get_embeddings(model, input_ids.unsqueeze(0)).detach()
+    full_embeds = torch.cat(
+        [
+            embeds[:, : input_slice.start, :],
+            input_embeds,
+            embeds[:, input_slice.stop :, :],
+        ],
+        dim=1,
+    )
+
+    logits = model(inputs_embeds=full_embeds).logits
+    targets = input_ids[target_slice]
+    loss = nn.CrossEntropyLoss()(logits[0, loss_slice, :], targets)
+
+    loss.backward()
+
+    grad = one_hot.grad.clone()
+    grad = grad / grad.norm(dim=-1, keepdim=True)
+
+    return grad
